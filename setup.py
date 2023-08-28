5ee6ed87cf35dba689700987629d56e4f03cff0f
duruyao <duruyao@gmail.com>
2023-08-28 16:36:32 +0800

    fix: fix function command_exists()

--- setup.py
+++ setup.py
@@ -7,68 +7,53 @@ from setuptools.command.install import install
 
 
 def command_exists(command):
-    try:
-        subprocess.run([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
+    from shutil import which
+    return which(command) is not None
 
 
 class CustomInstall(install):
     def run(self):
         # install python requirements
-        with open('requirements.txt') as req_file:
-            requirements = req_file.readlines()
-            for req in requirements:
-                req = req.strip()
-                if req:
-                    subprocess.run(['pip3', 'install', req])
+        try:
+            subprocess.run('pip3 install -r requirements.txt'.split(' '))
+        except Exception as e:
+            print(f'Error: {e}')
+            return
 
         # install os requirements
         platform = os.uname().sysname
         if platform == 'Linux':
-            if command_exists('apt-get'):
-                try:
-                    subprocess.run(['sudo', 'apt-get', 'install', 'ffmpeg'])
-                except Exception as e:
-                    print(f'Error: {e}')
-            elif command_exists('dnf'):
-                try:
-                    subprocess.run(['sudo', 'dnf', 'install', 'ffmpeg'])
-                except Exception as e:
-                    print(f'Error: {e}')
-            elif command_exists('yum'):
-                try:
-                    subprocess.run(['sudo', 'yum', 'install', 'ffmpeg'])
-                except Exception as e:
-                    print(f'Error: {e}')
-            elif command_exists('pacman'):
-                try:
-                    subprocess.run(['sudo', 'pacman', '-S', 'ffmpeg'])
-                except Exception as e:
-                    print(f'Error: {e}')
-            elif command_exists('zypper'):
-                try:
-                    subprocess.run(['sudo', 'zypper', 'install', 'ffmpeg'])
-                except Exception as e:
-                    print(f'Error: {e}')
-            elif command_exists('emerge'):
-                try:
-                    subprocess.run(['sudo', 'emerge', 'ffmpeg'])
-                except Exception as e:
-                    print(f'Error: {e}')
-            else:
-                print(f'Error: Unknown Linux distribution \'{platform}\', '
-                      f'see https://ffmpeg.org to install \'ffmpeg\' manually')
-        elif platform == 'Darwin':
             try:
-                subprocess.run(['brew', 'install', 'ffmpeg'])
-            except FileNotFoundError:
-                print('Error: \'brew\' required but not found, see https://brew.sh to install it first')
+                if command_exists('brew'):
+                    subprocess.run('brew install ffmpeg'.split(' '))
+                elif command_exists('apt-get'):
+                    subprocess.run('sudo apt-get install ffmpeg'.split(' '))
+                elif command_exists('dnf'):
+                    subprocess.run('sudo dnf install ffmpeg'.split(' '))
+                elif command_exists('yum'):
+                    subprocess.run('sudo yum install ffmpeg'.split(' '))
+                elif command_exists('pacman'):
+                    subprocess.run('sudo pacman -S ffmpeg'.split(' '))
+                elif command_exists('zypper'):
+                    subprocess.run('sudo zypper install ffmpeg'.split(' '))
+                elif command_exists('emerge'):
+                    subprocess.run('sudo emerge ffmpeg'.split(' '))
+                else:
+                    print(f'No package manager found, see https://ffmpeg.org to install ffmpeg manually')
             except Exception as e:
                 print(f'Error: {e}')
+                return
+        elif platform == 'Darwin':
+            try:
+                subprocess.run('brew install ffmpeg'.split(' '))
+            except FileNotFoundError:
+                print('Error: brew required but not found, see https://brew.sh to install it first')
+                return
+            except Exception as e:
+                print(f'Error: {e}')
+                return
         else:
-            print(f'No package manager found, see https://ffmpeg.org to install \'ffmpeg\' manually')
+            print(f'No package manager found, see https://ffmpeg.org to install ffmpeg manually')
 
         install.run(self)
 
