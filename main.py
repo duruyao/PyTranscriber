b1ae228b838d2ab63b85fc757181a382368e383e
duruyao <duruyao@gmail.com>
2023-08-28 23:07:25 +0800

    feat: show progress bar (not completed)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import argparse
 from typing import Tuple
 
+from tqdm import tqdm
 from pydub import AudioSegment
 import speech_recognition as sr
 
@@ -10,8 +11,8 @@ import speech_recognition as sr
 def parse_args() -> Tuple[str, str, str]:
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help='input media filename')
-    parser.add_argument('-o', '--output', required=False, type=str, help='output text filename')
-    parser.add_argument('--language', required=False, type=str, default='en-US', help='target language',
+    parser.add_argument('-o', '--output', required=False, type=str, help='output text filename (default: INPUT.out.txt)')
+    parser.add_argument('--language', required=False, type=str, default='en-US', help='target language (default: en-US)',
                         choices=['en-GB', 'en-US',
                                  'zh-CN', 'zh-TW'])
     args = parser.parse_args()
@@ -21,14 +22,13 @@ def parse_args() -> Tuple[str, str, str]:
     return input_filename, output_filename, target_language
 
 
-def transcribe_audio(input_filename: str, output_filename: str, target_language: str):
+def transcribe_audio(input_filename: str, output_filename: str, target_language: str) -> Tuple[bool, str]:
     temp_filename = f'{input_filename}.temp.wav'
     try:
         audio = AudioSegment.from_file(input_filename)
         audio.export(temp_filename, format='wav')
     except Exception as e:
-        print(f'Error: {e}')
-        return
+        return False, f'Error: {e}'
 
     recognizer = sr.Recognizer()
     with sr.AudioFile(temp_filename) as source:
@@ -37,14 +37,18 @@ def transcribe_audio(input_filename: str, output_filename: str, target_language:
         text = recognizer.recognize_google(audio_data, language=target_language)
         with open(output_filename, 'w', encoding='utf-8') as output_file:
             output_file.write(text)
-        print(f'Transcription Done! See {output_filename}')
+        return True, f'Transcription Done! See \'{output_filename}\''
     except sr.UnknownValueError:
-        print(f'Error: Cannot recognize language in {input_filename}')
-        return
+        return False, f'Error: Cannot recognize language in \'{input_filename}\''
     except sr.RequestError as e:
-        print(f'Error: {e}, check your net connection or proxy server setting')
-        return
+        return False, f'Error: {e}, check your net connection or proxy server setting'
 
 
 if __name__ == '__main__':
-    transcribe_audio(*parse_args())
+    err = ''
+    input, output, language = parse_args()
+    with tqdm(total=100, desc="Transcribing", unit="%", ncols=100) as pbar:
+        ok, err = transcribe_audio(input, output, language)
+        if ok:
+            pbar.update(100)
+    print(err)
